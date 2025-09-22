ea10d356667130b74427cba80b0a2dace14cd8f0
saparmar-conga <saparmar@conga.com>
2025-09-22 18:25:04 +0530

    Template changes - 202509.1.0

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, combineLatest, of } from 'rxjs';
-import { map, switchMap, delay, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ApiService } from '@congarevenuecloud/core';
 import { GuestUserService } from '@congarevenuecloud/ecommerce';
 import { AuthService } from './auth.service';
@@ -14,34 +14,10 @@ export class AuthorizationGuard {
   constructor(private authService: AuthService, private apiService: ApiService, private guestUserService: GuestUserService) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    // Add delay to prevent race condition between auth initialization and guest session setup
-    return of(null).pipe(
-      delay(100),
-      switchMap(() => {
-        // Check both auth status and guest authentication
-        return combineLatest([
-          this.authService.isAuthorized().pipe(catchError(() => of(false))),
-          this.apiService.isGuestAuthenticated().pipe(catchError(() => of(false)))
-        ]);
-      }),
+    return combineLatest([this.authService.isAuthorized(), this.apiService.isGuestAuthenticated()]).pipe(
       map(([isAuthorized, guestAuthenticated]) => {
-        // If user is already logged in, allow access
-        if (isAuthorized) {
-          return true;
-        }
-        
-        // If not guest authenticated, setup guest session
-        if (!guestAuthenticated) {
-          this.guestUserService.login();
-        }
-        
-        // Always allow access (ecommerce works in guest mode)
+        if (!guestAuthenticated) this.guestUserService.login();
         return true;
-      }),
-      catchError(() => {
-        // If any error occurs, setup guest session and continue
-        this.guestUserService.login();
-        return of(true);
       })
     );
   }
